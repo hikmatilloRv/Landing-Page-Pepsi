1a28e72c864e9e784f36397233849b205049a24e
hikmatillo <hkpt737@gmail.com>
2022-11-05 15:50:29 +0300

    added some files

---
+++ pepsi.js
@@ -0,0 +1,8 @@
+function imgSlider(anything) {
+    document.querySelector('.pepsi').src = anything;
+}
+
+function changeBgColor(color) {
+    let sec = document.querySelector('.sec');
+    sec.style.background = color;
+}
